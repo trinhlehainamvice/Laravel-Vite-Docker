a6a92f8cda3c89e13dab39a00701e51352f36667
trinhlehainamvice <125254137+trinhlehainamvice@users.noreply.github.com>
2024-06-07 19:44:24 +0900

    Add wait database connection to entrypoint.sh to guard laravel run migration before database connection.

--- laravel/entrypoint.sh
+++ laravel/entrypoint.sh
@@ -5,7 +5,9 @@ if [ ! -f /var/www/html/vendor/autoload.php ]; then
 fi
 
 if [ ! -f /var/www/html/.env ]; then
+    echo "Create .env file"
     cp /var/www/html/.env.example /var/www/html/.env
+    rm /var/www/html/.env.example
 fi
 
 # Check if APP_KEY is set in the .env file
@@ -16,11 +18,26 @@ else
   php artisan key:generate
 fi
 
+# TODO: read from .env
+DB_HOST=${DB_HOST:-mysql}
+DB_PORT=${DB_PORT:-3306}
+
+# NOTE: wait until can connect to database before running php artisan migrate
+# https://stackoverflow.com/questions/25503412/how-do-i-know-when-my-docker-mysql-container-is-up-and-mysql-is-ready-for-taking
+until nc -z $DB_HOST $DB_PORT
+do
+  echo "Waiting for database connection..."
+  # wait for 5 seconds before check again
+  sleep 5
+done
+
+echo "Database connection established."
+
 # Function to check if database is migrated or not
 is_database_not_migrated() {
   # Check if there are any entries in the migrations table
   # HACK: output from migrate:status potentially changed
-  php artisan migrate:status | grep 'not found'
+  php artisan migrate:status | grep 'not found' > /dev/null
 }
 
 if is_database_not_migrated; then
@@ -33,4 +50,3 @@ fi
 if ! pgrep -x "php-fpm" > /dev/null; then
     php-fpm
 fi
-
